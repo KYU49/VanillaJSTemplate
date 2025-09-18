fc2bd1bb32f4033681e832217e54c321947f983d
KYU <existence.m.p@gmail.com>
2025-09-18 12:33:52 +0900

    ライセンスの追記, radio buttonへの対応

--- DataBinding.js
+++ DataBinding.js
@@ -1,3 +1,28 @@
+/*!
+ * MIT License
+ * 
+ * Copyright (c) 2025 KYU @ https://github.com/KYU49
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
 export class DataBinding{
     /**
      * @typedef {[BoundElement]} #boundElements この変数にbindされているhtml elementと、bindの情報。
@@ -94,6 +119,21 @@ export class DataBinding{
                 },
                 "change"
             )
+        }
+    }
+    // input radioにbindしたい場合のテンプレート
+    // document.forms.formName.elements["radio_input_name"]を渡すこと。
+    static BoundRadio = class extends DataBinding.BoundElement{
+        constructor(element){
+            super(element, 
+                (newValue, element) => {
+                    element.value = newValue;
+                },
+                (parent, element) => {
+                    parent.value = element.value;
+                },
+                "change"
+            );
         }
     }
     // booleanの数値に対して、elementの可視化状態をbindしたい場合のテンプレート。別途cssで`.hidden{display: "none";}`の指定が必要
